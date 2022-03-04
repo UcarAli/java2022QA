f718824f3a83a95835623a8bd6d8f96a590ac4b0
M. Ali UCAR <ucaralifr@gmail.com>
2022-03-04 22:42:59 +0100

    22/03-12

--- src/day12_stringManipulation/C05_replace.java
+++ src/day12_stringManipulation/C05_replace.java
@@ -5,16 +5,17 @@ public class C05_replace {
 
         String str = "Bugun ne cok sey ogrendik";
 
-
+        // bu cumledeki bosluk disindaki karakter sayisini bulunuz
         System.out.println("space haric karakter sayisi : "+str.replace(" ", "").length());
 
-
+        // atama yapilmadigi surece orjinal String kalici olarak degismez
+        // sadece o satir icin degisiklik yapilip sonuc yazdirilmis olur
 
         System.out.println("orjinal str karakter sayisi : "+ str.length());
 
-
-
-
+        // str da kalici degisiklik yapalim
+        // bugun yerine yarin
+        // ogrendik yerine ogrenecegiz
 
         str = str.replace("Bugun", "Yarin");
         str = str.replace("ogrendik", "ogrenecegiz");
