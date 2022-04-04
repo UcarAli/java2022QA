5bdef873c3a5261bb94812538a8bbc5a67b32927
M. Ali UCAR <ucaralifr@gmail.com>
2022-04-04 18:49:02 +0200

    22/03-35

---
+++ src/day35_inheritance/Memur.java
@@ -0,0 +1,15 @@
+package day35_inheritance;
+
+public class Memur extends Personel{
+    public static void main(String[] args) {
+
+        Memur objIsci = new Memur();
+        objIsci.isim="Tuba";
+        objIsci.soyisim="Akdemir";
+        objIsci.adres="Baku";
+
+
+
+
+    }
+}
---
+++ src/day35_inheritance/Parent.java
@@ -0,0 +1,17 @@
+package day35_inheritance;
+
+public class Parent {
+    public static String isim = "Neval";
+    public static String soyisim = "Ulker";
+    protected static String fabrika = "Yildiz Tekstil";
+
+    public static void medhod1(){
+        System.out.println("medhod1");
+    }
+
+    protected static void medhod2(){
+        System.out.println("protected medhod2");
+    }
+
+
+}
